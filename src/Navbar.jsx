34e6b96bef87c27c9840d0ae2a03af774a6c6853
hyeji-neee <tlsgmlsk123@naver.com>
2025-05-18 03:01:29 +0900

    feat: Navbar 컴포넌트 구현

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/projects">프로젝트</Link>
+      <Link to="/about">자기소개</Link>
+    </nav>
+  );
 };
 
 export default Navbar;
