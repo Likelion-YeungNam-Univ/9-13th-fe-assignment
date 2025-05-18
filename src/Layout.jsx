8832877bcd364974494d0dbebf03fc77be503a5b
nyeonseok <123443488+nyeonseok@users.noreply.github.com>
2025-05-18 16:45:43 +0900

    layout 구현

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,10 +1,41 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 export default function Layout() {
+  const navItems = [
+    {
+      id: 1,
+      text: "Home",
+      path: "/",
+    },
+    {
+      id: 2,
+      text: "About Me",
+      path: "/about",
+    },
+    {
+      id: 3,
+      text: "Projects",
+      path: "/projects",
+    },
+  ];
   return (
-    <div>
-      <div>Layout</div>
-      <Outlet />
+    <div className="flex h-screen">
+      <nav className="bg-black w-80 flex justify-center">
+        <div className="flex flex-col w-4/5 space-y-20  py-10">
+          {navItems.map((item) => (
+            <Link
+              key={item.id}
+              to={item.path}
+              className="text-3xl text-white font-bold hover:no-underline "
+            >
+              {item.text}
+            </Link>
+          ))}
+        </div>
+      </nav>
+      <div className="flex-grow overflow-y-auto bg-white p-10">
+        <Outlet />
+      </div>
     </div>
   );
 }
