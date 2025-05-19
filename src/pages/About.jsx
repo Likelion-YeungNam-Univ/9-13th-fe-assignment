da9c615863a052b746c34d8b0effaebecf4b6963
hyeji-neee <tlsgmlsk123@naver.com>
2025-05-19 18:15:21 +0900

    feat: 자기소개 페이지 구현

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,44 @@
 import React from "react";
 
+const profileData = [
+  { key: "이름", value: "신혜진" },
+  { key: "생년월일", value: "2004-06-03" },
+  { key: "성별", value: "여" },
+  { key: "혈액형", value: "AB" },
+  { key: "MBTI", value: "ISTP" },
+  { key: "학교", value: "영남대학교" },
+  { key: "학부(학과)", value: "컴퓨터학부 소프트웨어융합전공" },
+  { key: "학년", value: "3학년" },
+];
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <h1 className="p-10 text-3xl text-center text-blue-950 font-extrabold">
+        About me . . .
+      </h1>
+      {/* 프로필*/}
+      <div className="mx-60">
+        <h2 className="text-xl text-blue-950 font-bold">Profile</h2>
+        <ul className="border-2 p-7 space-y-1 w-fit">
+          {profileData.map((item, key) => (
+            <li key={key} className="flex">
+              <span className="w-28 font-semibold">{item.key}</span>
+              <span>{item.value}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {/* 자기소개 */}
+      <div className="mx-60 my-14">
+        <h2 className="text-xl text-blue-950 font-bold">Self-Introduction</h2>
+        <p className="border-2 border-blue-950 p-4">
+          안녕하세요! 멋쟁이사자처럼 13기 프론트엔드 아기사자 신혜진입니다.
+          앞으로 열심히 하겠습니다!
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
