3ec5b59e79fa14b6d9de2f5453fc08d89d9ae3e5
hyeji-neee <tlsgmlsk123@naver.com>
2025-05-20 00:11:16 +0900

    feat: About 페이지에 프로필 사진 추가

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import profileImg from "../assets/profileImg.png";
 
 const profileData = [
   { key: "이름", value: "신혜진" },
@@ -18,21 +19,24 @@ const About = () => {
         About me . . .
       </h1>
       {/* 프로필*/}
-      <div className="mx-60">
-        <h2 className="text-xl text-blue-950 font-bold">Profile</h2>
-        <ul className="border-2 p-7 space-y-1 w-fit">
-          {profileData.map((item, key) => (
-            <li key={key} className="flex">
-              <span className="w-28 font-semibold">{item.key}</span>
-              <span>{item.value}</span>
-            </li>
-          ))}
-        </ul>
+      <div className="mx-60 flex justify-center items-start gap-12">
+        <div>
+          <h2 className="text-xl text-blue-950 font-bold">Profile</h2>
+          <ul className="bg-blue-950 text-gray-200 p-7 space-y-1 w-fit">
+            {profileData.map((item, key) => (
+              <li key={key} className="flex">
+                <span className="w-28 font-semibold">{item.key}</span>
+                <span>{item.value}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <img src={profileImg} className="pt-12 w-96 h-72 object-cover" />
       </div>
       {/* 자기소개 */}
       <div className="mx-60 my-14">
         <h2 className="text-xl text-blue-950 font-bold">Self-Introduction</h2>
-        <p className="border-2 border-blue-950 p-4">
+        <p className="border-3 border-blue-950 p-4">
           안녕하세요! 멋쟁이사자처럼 13기 프론트엔드 아기사자 신혜진입니다.
           앞으로 열심히 하겠습니다!
         </p>
