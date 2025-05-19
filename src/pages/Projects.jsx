9fe0dc790700b6582f49c700b3e7f0c4cf1643e0
nyeonseok <123443488+nyeonseok@users.noreply.github.com>
2025-05-19 19:33:55 +0900

    projects구현

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,3 +1,34 @@
+import { Link } from "react-router-dom";
+import vero from "../assets/vero.png";
+
 export default function Projects() {
-  return <div>Projects</div>;
+  const projects = [
+    {
+      id: 1,
+      title: "VERO",
+    },
+  ];
+  return (
+    <div className="flex flex-col w-full bg-white px-10 overflow-y-auto ">
+      <div className="text-4xl font-extrabold text-blue-600 border-b-4 border-blue-500 pb-2 mb-6">
+        Projects
+      </div>
+      <div className="flex">
+        {projects.map((p) => (
+          <Link
+            to={`/projects/${p.id}`}
+            state={p.title}
+            className="border-none rounded-lg w-96 h-64"
+          >
+            <div className="h-full flex flex-col">
+              <img src={vero} className="h-4/5 w-full rounded-t-lg" />
+              <div className="bg-[#262626] flex-grow rounded-b-lg flex items-center text-white font-bold text-lg pl-4 ">
+                {p.title}
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
