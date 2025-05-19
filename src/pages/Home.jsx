d3cd5c62a874f6e324be80ca347fa54dbe4e9cdf
hyeji-neee <tlsgmlsk123@naver.com>
2025-05-19 18:28:33 +0900

    feat: 홈 화면 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="text-center">
+      <h1 className="text-4xl text-blue-950 font-style: italic font-extrabold pt-16 pb-5">
+        Hyejin's PORTFOLIO
+      </h1>
+      <p className="pb-2">신혜진의 포트폴리오 페이지에 오신 것을 환영합니다!</p>
+      <p>
+        상단 네비게이션 바를 통해 프로젝트와 자기소개 페이지로 이동할 수
+        있습니다 :D
+      </p>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
