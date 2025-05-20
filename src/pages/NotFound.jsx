83ec77ef8fa98e016ea0b4b7b2b4dfcafa69ade9
gyeongsangseaman <sadx302@gmail.com>
2025-05-20 17:06:45 +0900

    NotFound 스타일 수정

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,23 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-gray-400 to-gray-100 text-center px-4">
-      <h1 className="text-9xl font-extrabold text-gray-500 drop-shadow-lg mb-4">
+    <div
+      onClick={() => navigate("/")}
+      className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-blue-200 to-red-200 text-center px-4"
+    >
+      <h1 className="text-9xl font-extrabold text-purple-400 drop-shadow-lg mb-4">
         404
       </h1>
-      <h2 className="text-2xl md:text-3xl font-semibold text-gray-900 mb-2">
-        페이지를 찾을 수 없습니다
+      <h2 className="text-2xl md:text-3xl font-semibold text-gray-700 mb-2">
+        Not Found
       </h2>
-      <p className="text-gray-600 mb-6">
-        요청하신 페이지가 존재하지 않거나, 이동되었을 수 있습니다.
-      </p>
-      <a
-        href="/"
-        className="px-6 py-3 bg-gray-500 hover:bg-gray-600 text-white font-medium rounded-xl shadow-md transition"
-      >
-        홈으로 돌아가기
-      </a>
+      <p className="text-gray-700 mb-6">화면을 클릭하세요.</p>
     </div>
   );
 };
