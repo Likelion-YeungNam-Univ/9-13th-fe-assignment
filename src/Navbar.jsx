6825c4aac5b8f942a43f32b230352f7176fd053e
Jiwon Park <parkjiwon0821@naver.com>
2025-05-20 20:02:27 +0900

    design: navbar 구현

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,31 @@
 import { Link } from "react-router-dom";
 
 const Navbar = () => (
-  <nav className="p-4 bg-gray-100 flex gap-4">
-    <Link to="/">홈</Link>
-    <Link to="/about">자기소개</Link>
-    <Link to="/projects">프로젝트</Link>
+  <nav className="flex justify-evenly sticky top-0 w-full bg-blue-600">
+    <div className="flex items-center">
+      <span className="text-xl font-bold text-white">박지원 포트폴리오</span>
+    </div>
+
+    <div className="flex space-x-4">
+      <Link
+        to="/"
+        className="px-3 py-2 font-medium text-white hover:bg-blue-700"
+      >
+        홈
+      </Link>
+      <Link
+        to="/about"
+        className="px-3 py-2 font-medium text-white hover:bg-blue-700"
+      >
+        자기소개
+      </Link>
+      <Link
+        to="/projects"
+        className="px-3 py-2 font-medium text-white hover:bg-blue-700"
+      >
+        프로젝트
+      </Link>
+    </div>
   </nav>
 );
 
