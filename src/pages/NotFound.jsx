8cec596558c5d04933bf8b0b22382c704929ccb7
minhyuk <martingodfamily@gmail.com>
2025-05-20 17:52:34 +0900

    9회차 과제 - 강민혁

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>404 - 페이지를 찾을 수 없습니다.</h1>
+      <p>요청하신 페이지는 존재하지 않거나 이동되었을 수 있습니당.</p>
+    </div>
+  );
+};
+
+export default NotFound;
---
+++ src/pages/about.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const about = () => {
+  return (
+    <div>
+      <h1>자기소개</h1>
+      <p>안녕하세요 저는 멋쟁이강민혁이라고 합니다.</p>
+      <p>
+        저에 대해서 소개를 하자면 저는 대구에서 태어났고 대구에서 살고 있고
+        대구에서 죽을 예정입니다.
+      </p>
+    </div>
+  );
+};
+
+export default about;
